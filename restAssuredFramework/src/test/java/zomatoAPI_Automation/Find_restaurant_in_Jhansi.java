f0a8c0121dffdc34b30e085870d3d24f8ab3a124
jit <jit@jit-Ideapad-Z570>
2020-03-08 23:25:54 +0530

    updated the new method

--- restAssuredFramework/src/test/java/zomatoAPI_Automation/Find_restaurant_in_Jhansi.java
+++ restAssuredFramework/src/test/java/zomatoAPI_Automation/Find_restaurant_in_Jhansi.java
@@ -42,4 +42,34 @@ public class Find_restaurant_in_Jhansi {
 		}
 		
 	}
+	
+	@Test()
+	public void ListofRestaurantInGurgaon() {
+		String CityName = "Gurgaon";
+		RestAssured.baseURI = "https://developers.zomato.com/api/v2.1/";
+		ValidatableResponse response = RestAssured.given().header("user-key", "a9a8b860396953c3de905e9ad8283dff").when().get("cities?q="+CityName)
+				.then().body("location_suggestions.name", Matchers.contains("Gurgaon"));
+		response.assertThat().statusCode(200);
+		
+		Response resp = RestAssured.given().header("user-key", "a9a8b860396953c3de905e9ad8283dff").when().get("cities?q="+CityName);
+		Object CityID = resp.body().jsonPath().get("location_suggestions.id");
+		
+		String city = CityID.toString().replaceAll("[\\[\\]]", "");
+		Response restaurants = RestAssured.given().header("user-key", "a9a8b860396953c3de905e9ad8283dff").when().get("location_details?entity_id="+city+"&entity_type=city");
+		
+		JsonPath temp = restaurants.jsonPath().setRoot("best_rated_restaurant.restaurant");
+		List<Object> address = temp.getList("location.address");
+		Iterator<Object> addcounter = address.iterator();
+		List<Object> Rest_Name = temp.getList("name");
+		
+		System.out.println("Total Number of Restaurant Avaible :: "+Rest_Name.size());
+		
+		Iterator<Object> counter = Rest_Name.iterator();
+		while(counter.hasNext())
+		{
+			System.out.println("Restaurant Name :: "+counter.next().toString());	
+			System.out.println("Address :: "+addcounter.next().toString());
+		}
+		
+	}
 }
